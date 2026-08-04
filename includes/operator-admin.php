e03cfda9bb886fdbfe8c2e74a971d2524a7073ee
Daniel Maksimov <bidpdxweb@gmail.com>
2026-08-04 07:42:51 -0700

    Add no-cost operator safeguard self-test

--- includes/operator-admin.php
+++ includes/operator-admin.php
@@ -455,6 +455,14 @@ function nwmd_directory_render_operator_admin_page() {
             ) {
                 nwmd_directory_render_operator_budget_section();
             }
+
+            if (
+                function_exists(
+                    'nwmd_directory_render_operator_budget_test_section'
+                )
+            ) {
+                nwmd_directory_render_operator_budget_test_section();
+            }
             ?>
             <h2><?php echo esc_html__('Run operator', 'local-directory-framework'); ?></h2>
 
